e42acf80f94e672e1c9d170f0c85a773bcb8f25f
lwohts <ohtsu-y1@libroworks.co.jp>
2016-05-19 14:15:51 +0900

    SVG書き出しの不具合修正（閉じタグの後に改行を追加）

--- app/lib/fileUtil.js
+++ app/lib/fileUtil.js
@@ -206,7 +206,7 @@ var fileUtil = {
       result += '<image width="' + printW + '" height="' + printH + '" ' 
             + 'xlink:href="' + $1 + '" '
             + 'transform="scale(' + newscale + ') translate(0,0)"> \n';
-      result += '</svg>';
+      result += '</svg> \n';
       console.log(result);
       return result;
     });
@@ -275,7 +275,7 @@ var fileUtil = {
       result += '<image width="' + printW + '" height="' + printH + '" ' 
             + 'xlink:href="' + $1 + '" '
             + 'transform="scale(' + newscale + ') translate(0,0)"> \n';
-      result += '</svg>';
+      result += '</svg> \n';
       console.log(result);
       return result;
     });
